783e31b8259f93efa71b9ba739416528ba6c8c60
ten951@qq.com <ten951@qq.com>
2017-03-15 14:25:20 +0800

    spring boot 第一天学习内容

---
+++ sprboot/src/main/java/com/wyt/sprboot/SprbootApplication.java
@@ -0,0 +1,12 @@
+package com.wyt.sprboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprbootApplication.class, args);
+	}
+}
