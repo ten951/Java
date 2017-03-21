380f2b21647b53dacccdf3a7c82166a7dd5fea41
ten951@qq.com <ten951@qq.com>
2017-03-21 16:24:15 +0800

    工厂方法模式:用模板方法模式来构建实例的工厂.

--- design/src/main/java/com/wyt/headfirst/thinking/factory/factory/idcard/IDCard.java
+++ design/src/main/java/com/wyt/headfirst/thinking/factory/factory/idcard/IDCard.java
@@ -4,13 +4,19 @@ import com.wyt.headfirst.thinking.factory.factory.framework.Product;
 
 /**
  * 继承产品类,实现如何使用卡
+ *
  * @author Darcy
  *         Created by Administrator on 2017/3/21.
  */
 public class IDCard extends Product {
     private String owner;
 
-    public IDCard(String owner) {
+    /**
+     * 私有化构造器,目的是无法通过new来构造对象,只能通过工厂方法createProduct构建
+     *
+     * @param owner 拥有着
+     */
+    IDCard(String owner) {
         System.out.println(" 制作" + owner + " 的ID卡");
         this.owner = owner;
     }
