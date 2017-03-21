5ea781bfe75c221b558c544024e0855c0cb2ea0f
ten951@qq.com <ten951@qq.com>
2017-03-21 14:30:36 +0800

    父类规定流程,在子类中实现的具体处理

--- design/src/main/java/com/wyt/headfirst/thinking/factory/method/AbstractDisplay.java
+++ design/src/main/java/com/wyt/headfirst/thinking/factory/method/AbstractDisplay.java
@@ -3,8 +3,7 @@ package com.wyt.headfirst.thinking.factory.method;
 /**
  * 模板方法模式,就是父类抽象方法,子类去实现具体行为.如(open,print,close)
  * 父类编写的算法(display) 可以复用.
- * 父类定义具体的流程,子类负责具体行为
- *
+ * 父类规定流程,在子类中实现的具体处理
  * @author Darcy
  *         Created by Administrator on 2017/3/21.
  */
