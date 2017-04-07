523dfaa03b46ab0138dcd511ecc0058eaca9256e
ten951@qq.com <ten951@qq.com>
2017-04-07 11:50:12 +0800

    Bridge模式,将类的功能层次结构合类的实现层次结构连接起来.

---
+++ design/src/main/java/com/wyt/headfirst/thinking/bridge/DisplayImpl.java
@@ -0,0 +1,13 @@
+package com.wyt.headfirst.thinking.bridge;
+
+/**
+ * @author Darcy
+ *         Created by Administrator on 2017/4/7.
+ */
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+
+    public abstract void rawPrint();
+
+    public abstract void rawClose();
+}
