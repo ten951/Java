6005d394dc7aba01d8c15339809daf86633bd981
Darcy <ten951@qq.com>
2017-07-20 17:18:50 +0800

    Ribbon 负载均衡搭建

--- learn/otps/restaurant-service/src/main/java/com/wyt/learn/RestaurantApp.java
+++ learn/otps/restaurant-service/src/main/java/com/wyt/learn/RestaurantApp.java
@@ -2,9 +2,10 @@ package com.wyt.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class RestaurantApp {
 
 	public static void main(String[] args) {
---
+++ learn/otps/ribbon-consumer/src/main/java/com/wyt/learn/ribbonconsumer/resources/ConsumerController.java
@@ -0,0 +1,23 @@
+package com.wyt.learn.ribbonconsumer.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Darcy
+ * Created by Darcy on 2017/7/20.
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+}
