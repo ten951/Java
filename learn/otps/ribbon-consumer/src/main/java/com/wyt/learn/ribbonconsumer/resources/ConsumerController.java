2c83ee0e065b1c8eb152e8320de3cf4d9e1f76b6
Darcy <ten951@qq.com>
2017-07-21 10:27:39 +0800

    Ribbon 负载均衡搭建

--- learn/otps/ribbon-consumer/src/main/java/com/wyt/learn/ribbonconsumer/resources/ConsumerController.java
+++ learn/otps/ribbon-consumer/src/main/java/com/wyt/learn/ribbonconsumer/resources/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.wyt.learn.ribbonconsumer.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
