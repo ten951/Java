97bc176c16e2ce7552153bc860844a27a9921c88
ten951 <ten951@qq.com>
2018-02-01 16:13:05 +0800

    Netty 解决TCP 粘包/拆包问题

--- learn/design/src/main/java/com/wyt/headfirst/web/chapter4/ChildChannelHandler.java
+++ learn/design/src/main/java/com/wyt/headfirst/web/chapter4/ChildChannelHandler.java
@@ -7,6 +7,8 @@ import io.netty.handler.codec.string.StringDecoder;
 
 
 /**
+ * LineBasedFrameDecoder+StringDecoder组合就是按照行切换的文本解码器,它被设计用来支持TCP的粘包和拆包的
+ *
  * @author Darcy
  * Created By Darcy on 2018/2/1 下午3:23
  */
